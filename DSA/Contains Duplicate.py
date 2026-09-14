9331ef63ed207f56f914e5e10c36f175e7f88cbd
Jeyadinesh <101107143+Jeyadinesh@users.noreply.github.com>
2026-09-14 18:52:59 +0530

    Implement containsDuplicate function to check for duplicates

--- DSA/Contains Duplicate.py
+++ DSA/Contains Duplicate.py
@@ -1,1 +1,7 @@
-
+def containsDuplicate(self, nums: List[int]) -> bool:
+    seen={}
+    for i,num in enumerate(nums):
+        if num in seen:
+            return True
+        seen[num]=i
+    return False
